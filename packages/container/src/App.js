841f2ceaf42dfd03ea2238a73eb020c6090dc3dc
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2022-12-03 17:59:01 -0600

    feat: Removed old nav

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { MarketingApp } from "./components/MarketingApp";
-import { Header } from "./components/Header";
+import Header from "./components/Header";
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <Header />
+      <MarketingApp />
     </BrowserRouter>
   );
 };
