1b1d3a2f35edfc78eed6ac0872d97afddb3f2e84
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2022-12-03 18:36:13 -0600

    feat: Create scoped styles for Container and Marketing apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { MarketingApp } from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "cnt",
+  });
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// !When building for production, we want to make sure that the class names are prefixed for this project/app
+const generateClassName = createGenerateClassName({
+  productionPrefix: "mk",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      {/* Styles provider has some setup for CSS in JS class naming */}
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
